49994c9455c749f6c5c5e93df4e8be0fe73350f4
kimpearl3599 <kimpearl3599@gmail.com>
2022-05-04 21:40:41 +0900

    connect habits component and make callback funtions

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,33 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
+  // 보여주기만 하는 컴포넌트이기 때문에 state 필요없음
+  // 콜백함수를 단순히 호출만 하는 컴포넌트
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
   };
-  handleIncrement = (event) => {
-    //state 오브젝트 안의 count를 증가시킨 뒤, state를 업데이트 해줌
-    // this.state.count += 1; 를 하면 업데이트 되었는지 알 수 없음
-    this.setState({ count: this.state.count + 1});
-  }
-  handleDecrement = (event) => {
-    const count = this.state.count -1;
-    // count를 변수화시키고 count가 0보다 작으면 0을 출력하라는 조건을 추가
-    this.setState({ count: count < 0 ? 0 : count });
-  }
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
   render() {
+    console.log(this.props.habit);
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-      <span className="habit-name">Reading</span>
-      <span className="habit-count">{this.state.count}</span>
-      <button className="habit-button habit-increase" onClick={this.handleIncrement}>
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
-        <i className="fas fa-minus-square"></i>
-      </button>
-      <button className="habit-button habit-delete">
-        <i className="fas fa-trash"></i>
-      </button>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
       </li>
     );
   }
---
+++ src/components/habits.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Habit from "./habit";
+
+class Habits extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    //habits을 복제
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habit[index].count - 1;
+    habit[index].count = count < 0 ? 0 : count;
+    this.setState({ habits: habits });
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits: habits });
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Habits;
